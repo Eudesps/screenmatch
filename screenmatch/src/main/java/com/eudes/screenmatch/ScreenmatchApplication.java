0eec7ec1eb9186ddaf757075838ca3665a027a5f
José Eudes <joseeudes969@gmail.com>
2025-11-17 15:57:56 -0300

    conexão com API

--- screenmatch/src/main/java/com/eudes/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/eudes/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.eudes.screenmatch;
 
+import com.eudes.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=f5fb160b");
+        System.out.println(json);
     }
 }
