2a4cadab57eb9a5f08b5816fc2af2954645e260e
José Eudes <joseeudes969@gmail.com>
2025-11-19 19:33:50 -0300

    modificação da record, tirando "votos"

--- screenmatch/src/main/java/com/eudes/screenmatch/models/DadosFilme.java
+++ screenmatch/src/main/java/com/eudes/screenmatch/models/DadosFilme.java
@@ -4,6 +4,5 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record DadosFilme(@JsonAlias("Title") String titulo,
                          @JsonAlias("totaleSeasons") Integer totalTemporadas,
-                         @JsonAlias("omdbRationg") String avaliacao,
-                         @JsonAlias("omdbVotes") String votos) {
+                         @JsonAlias("omdbRationg") String avaliacao) {
 }
