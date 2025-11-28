f21315743932edab49c94abcc7d9155e755b93c1
José Eudes <joseeudes969@gmail.com>
2025-11-28 15:46:50 -0300

    coments ex 006

--- screenmatch/src/main/java/com/eudes/screenmatch/exercicios/interfaces/Ex006.java
+++ screenmatch/src/main/java/com/eudes/screenmatch/exercicios/interfaces/Ex006.java
@@ -11,6 +11,8 @@ public class Ex006 {
     public static void main(String[] args) {
         List<String> nomes = Arrays.asList("Copo", "Jarro", "Alô");
 
+        //Nesse exemplo ele compara as 2 strings da lista e consegue dizer qual das duas é a primeira na ordem alfabética
+        //e o sort aplica essa mudança a lista
         nomes.sort((a,b) -> a.compareTo(b));
 
         System.out.println(nomes);
