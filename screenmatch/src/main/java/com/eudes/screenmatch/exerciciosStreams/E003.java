74de78a5f3c6d72145890eec7b27666725310410
José Eudes <joseeudes969@gmail.com>
2025-12-30 15:46:31 -0300

    Ex003

---
+++ screenmatch/src/main/java/com/eudes/screenmatch/exerciciosStreams/E003.java
@@ -0,0 +1,20 @@
+package com.eudes.screenmatch.exerciciosStreams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+//3 - Dada a lista de nomes abaixo, concatene-os separados por vírgula.
+//No código a seguir, há um exemplo prático do resultado esperado.
+public class E003 {
+    public static void main(String[] args) {
+        List<String> nomes = Arrays.asList("Alice", "Bob", "Charlie");
+        // Resultado Esperado: "Alice, Bob, Charlie"
+
+        String concatenados = nomes.stream()
+                        .collect(Collectors.joining(", "));
+
+        System.out.println(concatenados);
+    }
+}
