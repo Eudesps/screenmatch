d457133eba36972090a5cf2fc8c84a1ba6db0254
José Eudes <joseeudes969@gmail.com>
2026-02-04 14:12:01 -0300

    Optional coments

--- screenmatch/src/main/java/com/eudes/screenmatch/newsExercises/MainOptional.java
+++ screenmatch/src/main/java/com/eudes/screenmatch/newsExercises/MainOptional.java
@@ -7,6 +7,17 @@ public class MainOptional {
         //FORMAS DE LIDAR COM O APTIONAL
         //Optional<String> optional = Optional.empty();
         //Optional<String> optional = Optional.of("Valor");
+
+        //Se não se tem certeza se o valor é null ou não, é interessante usar o ofNullable
         Optional<String> optional = Optional.ofNullable(null); // Esse aceita um valor nulo sendo passado.
+
+        optional.ifPresentOrElse(op -> System.out.println(op), () -> System.out.println("Não existe valor"));
+
+        /*
+        if (optional.isPresent()) {
+            System.out.println("Existe Valor: " + optional.get());
+        } else {
+            System.out.println("Não existe valor");
+        }*/
     }
 }
